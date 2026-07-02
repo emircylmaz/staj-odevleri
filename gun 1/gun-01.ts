8b2c5a2428c72cc3abc99d67df7d528f4283b440
Emir Can Yılmaz <63395697+emircylmaz@users.noreply.github.com>
2026-07-02 23:37:41 +0300

    Add files via upload

---
+++ gun 1/gun-01.ts
@@ -0,0 +1,47 @@
+import { readFile } from 'fs/promises';
+
+type Role = 'admin' | 'developer' | 'intern';
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: Role;
+}
+
+interface Admin {
+type: 'admin';
+permissions: string[];
+}
+
+interface Developer {
+type: 'developer';
+permissions: string[];
+}
+
+interface Intern {
+    type: 'intern';
+    permissions: string[];
+}
+
+
+function filterByRole <T extends User> (users: T[], role: Role): T[] {
+    return users.filter(user => user.role === role);
+}
+
+
+async function readAndFilterUsers(filePath: string, selectedRole: Role) {
+    try {
+        const data = await readFile(filePath, 'utf-8');
+
+        const users = JSON.parse(data);
+
+        const filteredResult = filterByRole(users, selectedRole);
+
+        console.log(`--- ${selectedRole} ---`);
+        console.log(filteredResult);
+    }
+    catch (error) {
+        console.error("Error! File could not be found or JSON format is wrong.")
+    }
+}
