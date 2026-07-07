5181c832d52b03f985f7b332c785f208b595cdbc
Emir Can Yılmaz <63395697+emircylmaz@users.noreply.github.com>
2026-07-07 15:32:10 +0300

    Add files via upload

---
+++ gun 4/gun-04.ts
@@ -0,0 +1,61 @@
+abstract class Bildirim {
+    kanalAdi = "";
+    abstract gonder(mesaj: string) : void;
+}
+
+class EmailBildirim extends Bildirim {
+    kanalAdi = "Email";
+    gonder(mesaj:string) {
+        console.log(`[${this.kanalAdi}]: ${mesaj}`);
+    }
+}
+
+class SmsBildirim extends Bildirim {
+    kanalAdi = "Sms";
+    gonder(mesaj:string) {
+        console.log(`[${this.kanalAdi}]: ${mesaj}`);
+    }
+
+}
+
+class SlackBildirim extends Bildirim {
+    kanalAdi = "Slack";
+    gonder(mesaj:string) {
+        console.log(`[${this.kanalAdi}]: ${mesaj}`);
+    }
+
+}
+
+const list : Bildirim[] = [
+new EmailBildirim(),
+new SmsBildirim(),
+new SlackBildirim()
+];
+
+for (const Bildirim of list) {
+    Bildirim.gonder("Merhaba!");
+}
+
+class TercihliBildirim{
+    private kanallar: Bildirim[]
+
+    constructor(gelenKanallar: Bildirim[]) {
+        this.kanallar = gelenKanallar;
+    }
+
+    mesajGonder(tercih: string, mesaj: string) {
+        const bulunanKanal = this.kanallar.find(kanal => kanal.kanalAdi === tercih)
+
+        if(bulunanKanal) {
+            bulunanKanal.gonder(mesaj);
+        }
+        else {
+            throw new Error("Hatali kanal secimi.");
+        }
+    }
+
+}
+
+const tercihli = new TercihliBildirim(list);
+tercihli.mesajGonder("Sms", "Bu bir sms mesajidir.");
+tercihli.mesajGonder("Email", "Bu bir sms mesaji degildir.");
